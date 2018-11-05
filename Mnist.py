f869b7ef2619331ceac19e2893ee8e7f96f902f7
Ramiro Castro Martinez <ramicasmar@gmail.com>
2018-11-04 21:40:55 -0300

    Add files via upload

---
+++ Mnist.py
@@ -0,0 +1,81 @@
+import csv
+import numpy as np
+import NeuralNetwork as net
+import time
+
+
+def ReadCSV(filename):
+	print("\nLoading " + filename + " ...")
+
+	inputs = []
+	targets = []
+
+	with open(filename) as csvfile:
+		reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+		for row in reader:
+			y = np.zeros(10) + 0.01
+			y[int(row[0])] = 0.99
+			targets.append(y)
+			inputs.append((np.asfarray(row[1:])/255)*0.99 + 0.01)
+
+	return np.asarray(inputs), np.asarray(targets)
+
+inputs,targets = ReadCSV("mnist_train.csv")
+
+X_nodes = 784
+L1_nodes = 200
+Y_nodes = 10
+epochs = 10
+lr = 0.001
+
+nn = net.NeuralNetwork(X_nodes,L1_nodes,Y_nodes,lr)
+
+number_examples = len(inputs)
+
+print("\nTraining...")
+
+for e in range(epochs):
+	init_time = time.time()
+	err = 0
+
+	for i in range(number_examples):
+		error = nn.Train(inputs[i],targets[i])
+		err = err + error
+	err = err/number_examples
+	finish_time = time.time()
+	diff = round((finish_time - init_time),2)
+	time_to_finish = round(((epochs - e)*diff)/60,2)
+	print("Error: " + str(err) + " | EPOCH: " + str(e) + " | Time to finish: " + str(time_to_finish) + " mins")
+
+#nn.Save("MNIST")
+#print("Weights and biases saved.")
+
+x,y = ReadCSV("mnist_test.csv")
+
+test_examples = len(x)
+
+ok_predictions = 0
+
+for i in range(test_examples):
+	expected = np.argmax(y[i])
+	prediction = np.argmax(nn.Query(x[i]))
+	if expected==prediction:
+		ok_predictions += 1
+
+accuracy = round((ok_predictions/test_examples)*100,2)
+print("Accuracy on test data: " + str(accuracy) + "%")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ NeuralNetwork.py
@@ -0,0 +1,119 @@
+import numpy as np
+
+class NeuralNetwork:
+
+	def __init__(self,X_nodes,L1_nodes,Y_nodes,learning_rate):
+		self.Xnodes = X_nodes
+		self.L1nodes = L1_nodes
+		self.Ynodes = Y_nodes
+		self.alfa = learning_rate
+
+		np.random.seed(2231)
+
+		self.W_XL1 = np.random.rand(self.Xnodes,self.L1nodes)
+		self.W_L1Y = np.random.rand(self.L1nodes,self.Ynodes)
+
+		self.B1 = np.random.rand(self.L1nodes,1)
+		self.B2 = np.random.rand(self.Ynodes,1)
+
+		print("W_XL1: \n" + str(self.W_XL1))
+		print("W_L1Y: \n" + str(self.W_L1Y))
+		print()
+		print("B1: \n" + str(self.B1))
+		print("B2: \n" + str(self.B2))
+
+	def sigmoid(self,x):
+		return 1/(1+np.exp(-x))
+
+	def sigmoid_deriv(self,x):
+		return self.sigmoid(x)*(1-self.sigmoid(x))	
+
+	def relu(self,x):
+		c = np.zeros_like(x)
+		slope = 0.01
+		c[x>0] = x[x>0]
+		c[x<=0] = slope*x[x<=0]
+		return c
+
+	def relu_deriv(self,x):
+		x[x<=0] = 0.01
+		x[x>0] = 1.0
+		return x
+
+	def Query(self,xlist):
+		X = np.array(xlist, ndmin=2).T
+
+		Z1 = np.dot(self.W_XL1.T,X) + self.B1
+		A1 = self.relu(Z1)
+
+		Z2 = np.dot(self.W_L1Y.T,A1) + self.B2
+		Y = self.sigmoid(Z2)
+
+		return Y
+
+
+	def Train(self,xlist,tlist):
+		
+		X = np.array(xlist, ndmin=2).T
+		T = np.array(tlist, ndmin=2).T
+
+		#print("X:\n" + str(X))
+		#print("T:\n" + str(T))
+
+		############################## FEEDFORWARD ################################
+
+		Z1 = np.dot(self.W_XL1.T,X) + self.B1
+		A1 = self.relu(Z1)
+
+		Z2 = np.dot(self.W_L1Y.T,A1) + self.B2
+		Y = self.sigmoid(Z2)
+
+		############################### ERRORS ####################################
+
+		E_Y = T - Y
+		E_W1 = np.dot(self.W_L1Y,E_Y)
+
+		############################### DELTAS ###################################
+
+		delta_W_L1Y = np.dot(-self.alfa*A1,(E_Y*self.sigmoid_deriv(Y)).T)
+		delta_B2 = -self.alfa*E_Y*self.sigmoid_deriv(Y)
+
+		delta_W_XL1 = np.dot(-self.alfa*X,(E_W1*self.relu_deriv(A1)).T)
+		delta_B1 = -self.alfa*E_W1*self.relu_deriv(A1)
+       
+        ############################## UPDATES ###################################
+		
+		self.W_L1Y = self.W_L1Y - delta_W_L1Y
+		self.B2 = self.B2 - delta_B2
+
+		self.W_XL1 = self.W_XL1 - delta_W_XL1
+		self.B1 = self.B1 - delta_B1
+
+		return np.sum(np.absolute(E_Y))
+
+	def Save(self,name):
+		np.savetxt(name+'_W_XL1.txt', self.W_XL1, fmt='%f')
+		np.savetxt(name+'_W_L1Y.txt', self.W_L1Y, fmt='%f')
+		np.savetxt(name+'_B1.txt', self.B1, fmt='%f')
+		np.savetxt(name+'_B2.txt', self.B2, fmt='%f')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
